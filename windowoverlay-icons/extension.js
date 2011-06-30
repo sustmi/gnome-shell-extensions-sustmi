fcdecad4cbb9269ab89f9cf4f727d3819afcc0d6
Miroslav Šustek <sustmidown@centrum.cz>
2011-07-01 01:37:34 +0200

    Icon moved to bottom left window corner. Added opacity animation.

--- windowoverlay-icons/extension.js
+++ windowoverlay-icons/extension.js
@@ -3,9 +3,11 @@ const Shell = imports.gi.Shell;
 const St = imports.gi.St;
 
 const Main = imports.ui.main;
+const Tweener = imports.tweener.tweener;
 const Workspace = imports.ui.workspace;
 
-const WINDOWOVERLAY_ICON_SIZE = 22;
+
+const WINDOWOVERLAY_ICON_SIZE = 32;
 
 function injectToFunction(parent, name, func) {
     let origin = parent[name];
@@ -24,35 +26,59 @@ function main() {
         let tracker = Shell.WindowTracker.get_default();
         let apps = tracker.get_running_apps('');
         
+        this._applicationIconBox = new St.BoxLayout({ style_class: 'windowoverlay-application-icon' });
+        
+        let icon = null;
         for (let i = 0; i < apps.length; i++) {
             let windows = apps[i].get_windows();
             for (let j = 0; j < windows.length; j++) {
                 if (windows[j] == windowClone.metaWindow) {
-                    this._icon = apps[i].create_icon_texture(WINDOWOVERLAY_ICON_SIZE);
+                    icon = apps[i].create_icon_texture(WINDOWOVERLAY_ICON_SIZE);
                 }
             }
         }
-        if (this._icon == undefined) {
-            this._icon = new St.Icon({ icon_name: 'applications-other', icon_type: St.IconType.FULLCOLOR, icon_size: 22 });
+        if (icon === undefined) {
+            icon = new St.Icon({ icon_name: 'applications-other',
+                                 icon_type: St.IconType.FULLCOLOR,
+                                 icon_size: WINDOWOVERLAY_ICON_SIZE,
+                                 style_class: 'windowoverlay-application-icon' });
         }
-                
-        parentActor.add_actor(this._icon);
+        
+        this._applicationIconBox.set_opacity(200);
+        this._applicationIconBox.add_actor(icon);
+        
+        parentActor.add_actor(this._applicationIconBox);
     });
     
     injectToFunction(Workspace.WindowOverlay.prototype, 'hide', function() {
-        this._icon.hide();
+        this._applicationIconBox.hide();
     });
     injectToFunction(Workspace.WindowOverlay.prototype, 'show', function() {
-        this._icon.show();
+        this._applicationIconBox.show();
+    });
+    
+    injectToFunction(Workspace.WindowOverlay.prototype, '_onEnter', function() {
+        Tweener.addTween(this._applicationIconBox, { time: 0.2,
+                                                     opacity: 50,
+                                                     transition: 'linear' });
+    });
+    injectToFunction(Workspace.WindowOverlay.prototype, '_onLeave', function() {
+        Tweener.addTween(this._applicationIconBox, { time: 0.2,
+                                                     opacity: 200,
+                                                     transition: 'linear' });
     });
     
     injectToFunction(Workspace.WindowOverlay.prototype, 'updatePositions', function(cloneX, cloneY, cloneWidth, cloneHeight) {
-        this._icon.set_position(Math.floor(this.title.x - WINDOWOVERLAY_ICON_SIZE - 3), Math.floor(this.title.y));
-        this._icon.raise_top();
+        let icon = this._applicationIconBox;
+        
+        let iconX = cloneX + cloneWidth - icon.width - 3;
+        let iconY = cloneY + cloneHeight - icon.height - 3;
+        
+        icon.set_position(Math.floor(iconX), Math.floor(iconY));
     });
     
     injectToFunction(Workspace.WindowOverlay.prototype, '_onDestroy', function() {
-        this._icon.destroy();
+        this._applicationIconBox.destroy();
     });
 
 }
